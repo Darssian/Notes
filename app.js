0d7ce23649b68514621789278eaa84a6fbf029f4
Darssian <korkin.kirill.02@gmail.com>
2024-12-26 15:00:36 +0300

    Notes

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const notes = [
     completed: true,
   },
 ];
+
 function render() {
   listElement.innerHTML = "";
   if (notes.length === 0) {
@@ -29,7 +30,6 @@ function render() {
 render();
 
 createBtn.onclick = function () {
-  // inputElement.value = inputElement.value.trim() // говнокод
   if (inputElement.value.trim().length === 0) {
     return;
   }
@@ -37,7 +37,7 @@ createBtn.onclick = function () {
     title: inputElement.value,
     completed: false,
   };
-  notes.push(newNote); // добавляем новую заметку в конец массива
+  notes.push(newNote);
   render();
   inputElement.value = "";
 };
@@ -49,42 +49,69 @@ closeConfirm.forEach((el) => {
 });
 
 function confirm(index) {
-  console.log(index)
   confirmDelete.style.display = "flex";
   confirmYes.onclick = function () {
     notes.splice(index, 1);
-  render();
-  confirmDelete.style.display = "none";
+    render();
+    confirmDelete.style.display = "none";
   };
 }
 
 listElement.onclick = function (event) {
-  if (event.target.dataset.index) {
-    const index = parseInt(event.target.dataset.index);
-    const type = event.target.dataset.type;
+  const index = parseInt(event.target.dataset.index);
+  const type = event.target.dataset.type;
 
-    if (type === "toggle") {
-      notes[index].completed = !notes[index].completed;
-      console.log("toggle", index);
-    } else if (type === "delete") {
-      confirm(index);
-    }
-  }
+  if (type === "toggle") {
+    notes[index].completed = !notes[index].completed;
   render();
+  } else if (type === "delete") {
+    confirm(index);
+  } else if (type === "edit") {
+    enableEditMode(index);
+  } else if (type === "save") {
+    saveEdit(index);
+  } else if (type === "cancel") {
+    render();
+  }
+
 };
+
+function enableEditMode(index) {
+  const listItem = listElement.children[index];
+  const inputList = listItem.querySelector(".input_li");
+
+  inputList.style.display = "flex";
+}
+
+function saveEdit(index) {
+  const listItem = listElement.children[index];
+  const input = listItem.querySelector(".edit_input");
+
+  notes[index].title = input.value.trim();
+  render();
+}
 
 function getNoteTemplate(note, index) {
   return `<li
           class="list-group-item d-flex mb-1 mt-1 justify-content-between align-items-center"
         >
+        <div class="input_li">
+          <input type="text" class="edit_input form-control" data-index="${index}" value="${
+    note.title
+  }" >
+          <button class="edit-input_save btn btn-success btn-sm" data-index="${index}" data-type="save" ><img src="./img/icon/check.svg" alt=""></button>
+          <button class="edit-input_cancel btn btn-danger btn-sm" data-index="${index}" data-type="cancel" ><img src="./img/icon/close-icon.svg" alt=""></button>
+        </div>
           <span class="${
             note.completed ? "text-decoration-line-through" : ""
-          }">${note.title}</span>
-          <span>
-            <span class="btn btn-small btn-${
-              note.completed ? "warning" : "success"
-            }" data-index="${index}" data-type="toggle">&check;</span>
-            <span class="btn btn-small btn-danger" data-index="${index}" data-type="delete">&times;</span>
-          </span>
-        </li>`;
+          }">${note.title}
+        </span>
+        <span>
+          <button class="btn btn-small edit btn-info" data-index="${index}" data-type="edit"><img  src="/img/icon/edit.svg" alt=""></button>
+          <button class="btn btn-small btn-${
+            note.completed ? "warning" : "success"
+          }" data-index="${index}" data-type="toggle">&check;</button>
+          <button class="btn btn-small btn-danger" data-index="${index}" data-type="delete">&times;</button>
+        </span>
+      </li>`;
 }
